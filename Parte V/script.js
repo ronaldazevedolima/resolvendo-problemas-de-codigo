b9f6892c7d83099b864055675ec69b043c99b687
ronaldazevedolima <ronaldazevedolima@gmail.com>
2022-04-05 12:30:41 -0300

    criando arquivos parte 5

---
+++ Parte V/script.js
@@ -0,0 +1,82 @@
+const techProducts = [
+  {
+    product: {
+      id: 1, // numérico
+      name: 'computer', // string
+    },
+    info: {
+      price: 2100, // number
+      quantity: 4, // number
+    },
+  },
+  {
+    product: {
+      id: 2,
+      name: 'mouse',
+    },
+    info: {
+      price: 56,
+      quantity: 3,
+    },
+  },
+  {
+    product: {
+      id: 3,
+      name: 'monitor',
+    },
+    info: {
+      price: 589,
+      quantity: 5,
+    },
+  },
+  {
+    product: {
+      id: 4,
+      name: 'keyboard',
+    },
+    info: {
+      price: 78,
+      quantity: 4,
+    },
+  },
+  {
+    product: {
+      id: 5,
+      name: 'HD',
+    },
+    info: {
+      price: 350,
+      quantity: 2,
+    },
+  },
+  {
+    product: {
+      id: 6,
+      name: 'webcam',
+    },
+    info: {
+      price: 187,
+      quantity: 8,
+    },
+  },
+  {
+    product: {
+      id: 7,
+      name: 'mic',
+    },
+    info: {
+      price: 69,
+      quantity: 5,
+    },
+  },
+  {
+    product: {
+      id: 8,
+      name: 'headset',
+    },
+    info: {
+      price: 216,
+      quantity: 2,
+    },
+  },
+];
